5b0e6bacc870f3e8d7717d9f37359381c39350f9
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:11:30 +0000

    Jun 14, 2023, 5:41 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -42,6 +42,12 @@ const posts = [
                   >
                     {post.category.title}
                   </a>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.category.title}
+                  </a>
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
