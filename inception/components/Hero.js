f7cfd29faf17b60edcaed47e99e6d643ce8f51ca
SK <sarthakkarandikar03@gmail.com>
2023-06-14 11:42:59 +0000

    Jun 14, 2023, 5:12 PM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -132,7 +132,7 @@ export default function Example() {
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                
               </a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
