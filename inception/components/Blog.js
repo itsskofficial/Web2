89e7f4b1a4b70eb8c104ea427ffe21360d07ecb1
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:08:09 +0000

    Jun 14, 2023, 5:38 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -7,8 +7,8 @@ const posts = [
         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
       date: 'Mar 16, 2020',
       datetime: '2020-03-16',
-        category: { title: 'Marketing', href: '#' },
-      subcategory: {title: ''}
+       category: { title: 'Marketing', href: '#' },
+      subcategory: {title: 'Instagram'},
       author: {
         name: 'Michael Foster',
         role: 'Co-Founder / CTO',
