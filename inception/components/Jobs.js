38f70de69cbb0ac6cd5cfe7194137b8821b9bb9d
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:32:54 +0000

    Jun 14, 2023, 6:02 PM

--- inception/components/Jobs.js
+++ inception/components/Jobs.js
@@ -2,7 +2,7 @@ const links = [
     { name: 'Find jobs', href: '#' },
     { name: 'Seek an internship', href: '#' },
     { name: 'Get paid', href: '#' },
-    { name: 'Meet our leadership', href: '#' },
+    { name: 'Add ', href: '#' },
   ]
   const stats = [
     { name: 'Offices worldwide', value: '12' },
