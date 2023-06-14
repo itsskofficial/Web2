b2339a50a6ef084e8db7becd3242bbf26daddfe2
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:11:43 +0000

    Jun 14, 2023, 5:41 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -8,7 +8,7 @@ const posts = [
       date: 'Mar 16, 2020',
       datetime: '2020-03-16',
       category: { title: 'Marketing', href: '#' },
-      subcategory: {title: 'Instagram'},
+      subcategory: {title: 'Instagram', href=},
       author: {
         name: 'Michael Foster',
         role: 'Co-Founder / CTO',
