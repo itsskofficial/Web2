82abc66e7df5697acbe36a0bfc43a292158b6efd
SK <sarthakkarandikar03@gmail.com>
2023-06-14 11:37:01 +0000

    Jun 14, 2023, 5:07 PM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -3,7 +3,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Pr', href: '#' },
+  { name: 'Blog', href: '#' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
