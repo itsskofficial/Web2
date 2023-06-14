062bbde5f436bfafec82329ceaf81991977a81a5
SK <sarthakkarandikar03@gmail.com>
2023-06-14 11:47:59 +0000

    Jun 14, 2023, 5:17 PM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -13,7 +13,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white">
+    <div className="bg-dark">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
