969833fac08960d9caf38ecacc246b51d64c3d86
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:17:22 +0000

    Jun 14, 2023, 5:47 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -30,7 +30,7 @@ const posts = [
               Learn how to grow your business with our expert advice.
             </p>
           </div>
-        
+			
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
