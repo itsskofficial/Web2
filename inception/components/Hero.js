04d973e5ce0006e3c0dbe7eee42b39f34611d660
SK <sarthakkarandikar03@gmail.com>
2023-06-14 11:39:49 +0000

    Jun 14, 2023, 5:09 PM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -113,7 +113,7 @@ export default function Example() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
+              Announcing our own blog .{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
