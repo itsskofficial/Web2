449323e92c6ff4fe59cf5130f368cb7df8c8b365
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:33:22 +0000

    Jun 14, 2023, 6:03 PM

--- inception/components/Jobs.js
+++ inception/components/Jobs.js
@@ -5,7 +5,7 @@ const links = [
     { name: 'Add experience to your resume', href: '#' },
   ]
   const stats = [
-    { name: 'Offices worldwide', value: '12' },
+    { name: '', value: '12' },
     { name: 'Full-time colleagues', value: '300+' },
     { name: 'Hours per week', value: '40' },
     { name: 'Paid time off', value: 'Unlimited' },
