e3320d5ab5ce2191244debb3a99b9dbfad8aa50d
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:08:22 +0000

    Jun 14, 2023, 5:38 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -17,7 +17,6 @@ const posts = [
           'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
     },
-    // More posts...
   ]
   
   export default function Example() {
