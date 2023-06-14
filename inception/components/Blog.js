cf634567941c2d34b5502af9aee26fe39c75ee1a
SK <sarthakkarandikar03@gmail.com>
2023-06-14 12:16:54 +0000

    Jun 14, 2023, 5:46 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -23,7 +23,7 @@ const posts = [
   export default function Blog() {
     return (
       <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 flex">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
