f859930e80453eafeff10a479d1d294b89fcdd3d
SK <sarthakkarandikar03@gmail.com>
2023-06-14 11:40:49 +0000

    Jun 14, 2023, 5:10 PM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -113,7 +113,7 @@ export default function Example() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our own new blog.{' '}
+              Announcing our own new website.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -122,7 +122,7 @@ export default function Example() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              The only blog you need for placements
+              The only site you need for placements
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
