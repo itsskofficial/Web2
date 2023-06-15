7a6621588406d9ed136d83e6271c47bac2b32173
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:56:14 +0000

    Jun 15, 2023, 12:26 PM

--- inception/components/Features.js
+++ inception/components/Features.js
@@ -45,13 +45,13 @@ export default function Example() {
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-white-900">
+                <dt className="text-base font-semibold leading-7 text-white">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                <dd className="mt-2 text-base leading-7 text-white">{feature.description}</dd>
               </div>
             ))}
           </dl>
