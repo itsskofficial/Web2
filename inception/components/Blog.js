e800a4ec9837c581759006be6c768a65b86f6192
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:34:06 +0000

    Jun 15, 2023, 12:04 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -154,7 +154,7 @@ export default function Blog() {
                         {({ open }) => (
                           <>
                             <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-white-500">
+                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
                                 <span className="font-medium text-white-900">{section.name}</span>
                                 <span className="ml-6 flex items-center">
                                   {open ? (
