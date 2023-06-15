36d5edcbc65c31bb15a15d8f8cef4fc268705a9e
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:59:15 +0000

    Jun 15, 2023, 11:29 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -163,11 +163,11 @@ export default function Blog() {
                                       defaultValue={option.value}
                                       type="checkbox"
                                       defaultChecked={option.checked}
-                                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                      className="h-4 w-4 rounded border-white-300 text-indigo-600 focus:ring-indigo-500"
                                     />
                                     <label
                                       htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                      className="ml-3 min-w-0 flex-1 textwhite-500"
+                                      className="ml-3 min-w-0 flex-1 text-white-500"
                                     >
                                       {option.label}
                                     </label>
@@ -188,7 +188,7 @@ export default function Blog() {
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900">New Arrivals</h1>
+            <h1 className="text-4xl font-bold tracking-tight text-white-900">New Arrivals</h1>
 
             <div className="flex items-center">
               <Menu as="div" className="relative inline-block text-left">
