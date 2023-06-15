0b3cfa9c8ea64852734ed15d72fb6dcd03be7dc1
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:30:11 +0000

    Jun 15, 2023, 12:00 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -132,7 +132,7 @@ export default function Blog() {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-dark py-4 pb-12 shadow-xl">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-dark py-4 pb-12 shadow-xl z-99">
                   <div className="flex items-center justify-between px-4">
                     <h2 className="text-lg font-medium text-white-900">Filters</h2>
                     <button
