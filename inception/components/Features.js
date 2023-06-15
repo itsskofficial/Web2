e24ea59d6bf8a8c4aad6e66d72147bbde66f99a1
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:55:40 +0000

    Jun 15, 2023, 12:25 PM

--- inception/components/Features.js
+++ inception/components/Features.js
@@ -33,7 +33,7 @@ export default function Example() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">
             Everything you need to get a job
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
