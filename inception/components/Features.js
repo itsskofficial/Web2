cd2d214c9c03bec4ffe0f479c1d59efc6bdba781
SK <sarthakkarandikar03@gmail.com>
2023-06-15 07:01:54 +0000

    Jun 15, 2023, 12:31 PM

--- inception/components/Features.js
+++ inception/components/Features.js
@@ -20,7 +20,7 @@ const features = [
     icon: ArrowPathIcon,
   },
   {
-    name: '',
+    name: 'Step by step training',
     description:
       'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
     icon: FingerPrintIcon,
