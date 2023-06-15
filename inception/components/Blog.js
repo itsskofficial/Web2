b7b1bda3da07c57a7874c3e0b094e74a9518c199
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:57:27 +0000

    Jun 15, 2023, 11:27 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -98,7 +98,7 @@ export default function Blog() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
+              <div className="fixed inset-0 bg-dark bg-opacity-25" />
             </Transition.Child>
 
             <div className="fixed inset-0 z-40 flex">
@@ -113,7 +113,7 @@ export default function Blog() {
               >
                 <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
                   <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                    <h2 className="text-lg font-medium text-white-900">Filters</h2>
                     <button
                       type="button"
                       className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
