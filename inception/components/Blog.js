5d6fd8045f27c1f7434c8e8ff818c92b567a1916
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:09:36 +0000

    Jun 15, 2023, 11:39 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -23,7 +23,7 @@ const posts = [
           'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
     {
-      id: 1,
+      id: 2,
       title: 'Boost your conversion rate',
       href: '#',
       description:
