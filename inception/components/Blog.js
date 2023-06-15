cd4193105e03b71dfb7fae20967c26ca89d191e2
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:53:58 +0000

    Jun 15, 2023, 11:23 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -84,7 +84,7 @@ function classNames(...classes) {
 export default function Blog() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
     return (
-      <div className="bg-white w-full">
+      <div className="bg-dark w-full">
       <div>
         {/* Mobile filter dialog */}
         <Transition.Root show={mobileFiltersOpen} as={Fragment}>
