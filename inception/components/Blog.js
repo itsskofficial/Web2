8ff89ff5dc0572986a78dc1337c974e06cff26ec
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:35:54 +0000

    Jun 15, 2023, 12:05 PM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -134,7 +134,7 @@ export default function Blog() {
               >
                 <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl z-99">
                   <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-white-900">Filters</h2>
+                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
                     <button
                       type="button"
                       className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-900"
