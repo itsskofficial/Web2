937ef3f44f74a59ca03094b7b14e0cb807aaf549
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:25:48 +0000

    Jun 15, 2023, 10:55 AM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -121,7 +121,7 @@ export default function Hero() {
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tight text-white-900 sm:text-6xl">
               The only site you need for placements
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
