cf43a66df61ed5672a50b1db7ce855052ce99edf
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:31:11 +0000

    Jun 15, 2023, 11:01 AM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -17,7 +17,7 @@ export default function Hero() {
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="#homr" className="-m-1.5 p-1.5">
               <span className="sr-only">OST Placements</span>
               <img
                 className="h-8 w-auto"
