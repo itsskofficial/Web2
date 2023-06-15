66fb272ba4b5a4938629357e7cba87be23380059
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:25:37 +0000

    Jun 15, 2023, 10:55 AM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -112,7 +112,7 @@ export default function Hero() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-white-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-white-600 ring-1 ring-white-900/10 hover:ring-white-900/20">
               Introducing us in a brand new way.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
