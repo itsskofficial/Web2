92f480bb35e4287629e80625b5cf71bd2b1d066b
SK <sarthakkarandikar03@gmail.com>
2023-06-15 04:54:39 +0000

    Jun 15, 2023, 10:24 AM

--- inception/pages/index.js
+++ inception/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Hero from '@/components/Hero'
 import Blog from '@/components/Blog'
+import Jobs from '@/components/Jobs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function Home() {
       <main className={styles.main}>
         <Hero />
         <Blog />
-        
+        <Jobs />
       </main>
     </>
   )
