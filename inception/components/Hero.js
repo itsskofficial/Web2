58b370a4023e6b5572da71b57a227773b80eda94
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:24:40 +0000

    Jun 15, 2023, 10:54 AM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -77,7 +77,7 @@ export default function Hero() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50"
                     >
                       {item.name}
                     </a>
@@ -86,7 +86,7 @@ export default function Hero() {
                 <div className="py-6">
                   <a
                     href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50"
                   >
                     Log in
                   </a>
