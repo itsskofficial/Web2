ac9e3e8fb58377f0ebf83afef25879f1091324c7
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:59:34 +0000

    Jun 15, 2023, 11:29 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -193,7 +193,7 @@ export default function Blog() {
             <div className="flex items-center">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
-                  <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                  <Menu.Button className="group inline-flex justify-center text-sm font-medium text-white-700 hover:text-gray-900">
                     Sort
                     <ChevronDownIcon
                       className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
