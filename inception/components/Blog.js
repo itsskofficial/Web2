6592e9d8ef69721cc2a7a98eb839ba2318cdce8c
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:15:30 +0000

    Jun 15, 2023, 11:45 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -350,7 +350,7 @@ export default function Blog() {
 
               {/* Product grid */}
                 <div className="lg:col-span-3">{/* Your content */}
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 pt-10 sm:mt-5 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
