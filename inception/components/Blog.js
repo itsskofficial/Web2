8cc223f824fea0b797f0fd9365d7e36159bd06af
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:45:44 +0000

    Jun 15, 2023, 11:15 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -323,7 +323,7 @@ export default function Blog() {
               </section>
             </main>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {/* <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
                 <div className="flex items-center gap-x-4 text-xs">
@@ -364,9 +364,9 @@ export default function Blog() {
                     <p className="text-gray-600">{post.author.role}</p>
                   </div>
                 </div>
-              </article>
-            ))}
-          </div>
+              </article> */}
+            {/* ))}
+          </div> */}
         </div>
       </div>
     )
