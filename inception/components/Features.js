0acc11de74c7902863c65f4c4c92a8a29f2418e0
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:55:17 +0000

    Jun 15, 2023, 12:25 PM

--- inception/components/Features.js
+++ inception/components/Features.js
@@ -34,7 +34,7 @@ export default function Example() {
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Everything you need to deploy your app
+            Everything you need to get a job
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
