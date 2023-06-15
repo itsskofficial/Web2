654238a2c256240cb9e435746e2b4773bf299322
SK <sarthakkarandikar03@gmail.com>
2023-06-15 06:11:36 +0000

    Jun 15, 2023, 11:41 AM

--- inception/components/Blog.js
+++ inception/components/Blog.js
@@ -365,13 +365,13 @@ export default function Blog() {
                 </a>
               </div>
               <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-white-900 group-hover:text-gray-600">
                   <a href={post.href}>
                     <span className="absolute inset-0" />
                     {post.title}
                   </a>
                 </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-white-600">{post.description}</p>
               </div>
               <div className="relative mt-8 flex items-center gap-x-4">
                 <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
