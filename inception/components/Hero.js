8d66680b0134ff7a26223f21d06d9913af05ac91
SK <sarthakkarandikar03@gmail.com>
2023-06-15 05:22:23 +0000

    Jun 15, 2023, 10:52 AM

--- inception/components/Hero.js
+++ inception/components/Hero.js
@@ -38,13 +38,13 @@ export default function Hero() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white-900">
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="#" className="text-sm font-semibold leading-6 text-white-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
