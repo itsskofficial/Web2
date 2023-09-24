c7ea2bd96b34604d6992c1ca0820890d09f095a1
SK <sarthakkarandikar03@gmail.com>
2023-09-24 09:08:26 +0000

    Sep 24, 2023, 2:38 PM

--- blog/app/layout.js
+++ blog/app/layout.js
@@ -11,12 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="container">
+      <body className = {inter.className}>
+        <div className = "container">
           {children}
         </div>
       </body>
-      <div class
+      
     </html>
   )
 }
